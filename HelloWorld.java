b3ea753d1d13ef85db22606cd123e90e63103bce
mooli476 <53701566+mooli476@users.noreply.github.com>
2023-11-30 11:40:00 +0530

    Update HelloWorld.java
    3rd commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
+        System.out.println("Hello Allu!"); 
         System.out.println("Hi sudha!");
         System.out.println("Hi chidvi!");
     }
